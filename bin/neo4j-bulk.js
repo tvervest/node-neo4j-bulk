1e35a0c23cce4cc6a7b8c38dfdb23e46ff987724
Thomas Vervest <thomas@squarewolf.nl>
2015-03-10 01:04:40 +0100

    refactored cli script to use https://github.com/chriso/cli //TODO: write mocha test cases to confirm proper operation in all situations //TODO: write CLI documentation

--- bin/neo4j-bulk.js
+++ bin/neo4j-bulk.js
@@ -3,44 +3,73 @@
 var Bulk = require('../index.js');
 var path = require('path');
 var fs = require('fs');
+var cli = require('cli');
+var pkg = require('../package.json');
 
-function updateDB(host, bulkObject) {
-    Bulk.generateClearBatch(function(e) {
-        if(e) console.log(e);
-        
-        console.log('Database cleared');
+cli.setApp(pkg.name, pkg.version);
 
-        Bulk.executeBatch(host, bulkObject, function(e) {
-            if(e) console.log(e);
+cli.enable('version');
 
-            console.log('Database has been updated');
-        });
-    });
-}
+cli.parse({
+	database: [false, 'Database protocol, hostname, and port for the target Neo4j instance (defaults to http://127.0.0.1:7474)'],
+	batch: [false, 'Source batch file', 'path'],
+	clear: ['c', 'Clears the database before executing the batch file'],
+	force: ['f', 'Disables security questions for destructive operations'],
+});
 
-/**
-* CLI Interface
-*/
+cli.main(function (args, options) {
+	if (!options.batch) this.fatal('no batch file specified (see --help)');
 
-function help() {
-    console.log('Please specify path to the database script');
-    console.log('Usage: node app <database file>');
-}
+	var host = options.database || 'http://127.0.0.1:7474';
 
-if(!process.argv[2]) {
-    help();
-    return 0;
-}
+	function executeBatch() {
+		var batchData;
+		try {
+			batchData = require(path.resolve(process.cwd(), options.batch));
+			if (batchData instanceof Bulk) {
+				batchData = batchData.generateBatch();
+			}
+		} catch (e) {
+			cli.fatal('Could not load batch file: ' + e.message);
+		}
 
-var filepath = path.resolve(process.cwd(), process.argv[2]);
+		Bulk.executeBatch(host, batchData, function(e) {
+			if(e) cli.fatal('Could not apply batch file: ' + e.message);
 
-if (fs.existsSync(filepath)) {
-    var bulkObject = require(filepath);
-    
-    console.log(bulkObject);
+			cli.ok('Database has been updated');
+		});
+	}
 
-    updateDB('http://127.0.0.1:7474', bulkObject);
-} else {
-    console.log('File not exist');
-    help();
-}
+	if (options.clear) {
+		function executeCleanBatch() {
+			Bulk.executeBatch(host, Bulk.generateClearBatch(), function (e) {
+				if (e) cli.fatal('Clearing the database failed: ' + e.message);
+
+				cli.ok('Database cleared');
+
+				executeBatch();
+			});
+		}
+
+		if (options.force) {
+			executeCleanBatch();
+		} else {
+			(function confirmAction() {
+				cli.info('WARNING: this operation will clear all data in the database.');
+				cli.info('Are you sure you want to continue? [y/N]');
+				process.stdin.on('data', function (text) {
+					text = text.toString();
+					if (text.toLowerCase() === 'y\n') {
+						executeCleanBatch();
+					} else if (text.toLowerCase() === 'n\n') {
+						cli.fatal('Cancelling operation');
+					} else {
+						confirmAction();
+					}
+				});
+			})();
+		}
+	} else {
+		executeBatch();
+	}
+});
